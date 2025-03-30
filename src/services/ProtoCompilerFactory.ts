3fc18b6e92d17fb1f26b62746318d1a9e445252e
mehmet.solak <solakmehmet02@gmail.com>
2025-03-30 18:30:47 +0300

    java integration (?)

--- src/services/ProtoCompilerFactory.ts
+++ src/services/ProtoCompilerFactory.ts
@@ -2,6 +2,7 @@ import { IProtoCompiler } from '../compilers/IProtoCompiler';
 import { GoProtoCompiler } from '../compilers/GoProtoCompiler';
 import { PythonProtoCompiler } from '../compilers/PythonProtoCompiler';
 import { SupportedLanguages } from '../constants/SupportedLanguages';
+import { JavaProtoCompiler } from '../compilers/JavaProtoCompiler';
 
 class ProtoCompilerFactory {
     static createCompiler(type: SupportedLanguages, protoPath: string): IProtoCompiler {
@@ -10,6 +11,8 @@ class ProtoCompilerFactory {
                 return new GoProtoCompiler(protoPath);
             case SupportedLanguages.Python:
                 return new PythonProtoCompiler(protoPath);
+            case SupportedLanguages.Java:
+                return new JavaProtoCompiler(protoPath);
             default:
                 throw new Error('Unsupported compiler type');
         }
